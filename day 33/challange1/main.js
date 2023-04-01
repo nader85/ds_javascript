f2f88063d95540e05fc940110a31f3e16e26bedf
nader85 <nader.etemi@gmail.com>
2023-04-01 10:33:11 +0200

    end of day 33

--- day 33/challange1/main.js
+++ day 33/challange1/main.js
@@ -1,17 +1,34 @@
 var circle = document.getElementById("circle");
+var timestart = 0;
+var time = document.getElementById("time");
 
 function showCircle()
 {
 	circle.style.display = 'block';
 	circle.style.top = Math.random() * 500 + 'px';
 	circle.style.left = Math.random() * 900 + 'px';
-	//circle.style.backgroundColor = randomColor();
-	//timestart = new Date().getTime();
+	circle.style.backgroundColor = randomColor();
+	timestart = new Date().getTime();
 
 }
 showCircle();
 
+function randomColor() {
+	var s = '0123456789ABCDEF';
+	var color = '#';
+
+	for (var i = 0; i < 6; i++) {
+		color = color + s[Math.floor(Math.random( )* 16)]
+	}
+	
+	return color;
+
+}
+
 circle.onclick = function(){
 circle.style.display = 'none';
 setTimeout(showCircle,2000);
+timeend = new Date().getTime();
+var time = (timeEnd - timestart )/1000;
+time.innerHTML = time +'s';
 }
